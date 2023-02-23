52098fd744700faf53e86d0dfef8a7697de2ed0f
Pranav C <pranavxc@gmail.com>
2023-02-23 11:35:39 +0530

    fix(gui): show all db options if uidt is SpecificDBType
    Signed-off-by: Pranav C <pranavxc@gmail.com>

--- packages/nc-gui/components/smartsheet/column/EditOrAdd.vue
+++ packages/nc-gui/components/smartsheet/column/EditOrAdd.vue
@@ -223,7 +223,10 @@ useEventListener('keydown', (e: KeyboardEvent) => {
             v-model:value="formState"
           />
 
-          <LazySmartsheetColumnAdvancedOptions v-model:value="formState" :advanced-db-options="advancedDbOptions" />
+          <LazySmartsheetColumnAdvancedOptions
+            v-model:value="formState"
+            :advanced-db-options="advancedDbOptions || formState.uidt === UITypes.SpecificDBType"
+          />
         </div>
       </Transition>
 
